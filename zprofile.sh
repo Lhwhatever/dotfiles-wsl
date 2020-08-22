cb6e66a2822db03aad508e0c3ac726d8c259b9db
Lhwhatever <llinhong.2301+github@gmail.com>
2020-08-22 16:38:03 +0800

    Add go binaries to $PATH

--- zprofile.sh
+++ zprofile.sh
@@ -1,1 +1,10 @@
 export BAT_THEME="Monokai Extended Origin"
+
+pathadd() {
+    PATHADD=$(IFS=: echo "$*")
+    export PATH="$PATH:$PATHADD"
+}
+
+if [ -d /usr/local/go ]; then
+    pathadd /usr/local/go/bin
+fi
